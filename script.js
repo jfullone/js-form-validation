5cb7fa8fc2ce2590a6fb07969d65cb3a85b27b84
Jayme Fullone <jayme.fullone@yahoo.com>
2021-05-26 11:18:17 -0400

    changes

--- script.js
+++ script.js
@@ -4,29 +4,17 @@ const init = function(){
 }
 
 const reset = function(ev){
-    //HTML will automatically put the form back to its initial state
-    //unless we do 
-    ev.preventDefault();
-    // programmatically we can reset it 
+    ev.preventDefault(); 
     document.getElementById('form-user').reset();
-    //if you want to do anything else...
 }
 
 const send = function(ev){
     ev.preventDefault(); 
     ev.stopPropagation();
-    //or the click will travel to the form and the form will submit
     let fails = validate();
-    //IF we wanted to do some async things then use a Promise with .then and .catch
     if(fails.length === 0){
-        //good to go
         document.getElementById('form-user').submit();
     }else{
-        //there are some errors to display
-        //bad user
-        //let err = document.querySelector('.error');
-        //let input = err.querySelector('input');
-        //err.setAttribute('data-errormsg', ` ... Missing ${input.placeholder}`);
         fails.forEach(function(obj){
             let field = document.getElementById(obj.input);
             field.parentElement.classList.add('error');
@@ -36,30 +24,20 @@ const send = function(ev){
 }
 
 const validate = function(ev){
-    //let valid = true;
     let failures = [];
-    //checkbox (or radio buttons grouped by name)
     let chk = document.getElementById('gender');
-    // .checked .value
     if(!chk.checked){
-        //valid = false;
-        //chk.parentElement.classList.add('error');
-        //chk.parentElement.setAttribute('data-errormsg', 'Must be alive to submit.');
         failures.push({input: 'gender', msg: 'Required'})
     }
 
-    //select
     let select = document.getElementById('input-age');
-    // .selectedIndex  .options  .length   .selectedValue  .value
     if( select.selectedIndex === 0 ){
         failures.push({input:'input-age', msg:'Too young'})
     }
 
-    //inputs for text, email, tel, color, number...
     let first = document.getElementById('input-first');
     let password = document.getElementById('input-password');
     let email = document.getElementById('input-email');
-    //.value, .defaultValue, length of value
     if( first.value === ""){
         failures.push({input:'input-first', msg:'Required Field'})
     } 
@@ -69,8 +47,7 @@ const validate = function(ev){
     if( email.value === ""){
         failures.push({input:'input-email', msg:'Required Field'})
     }
-    
-    //return a boolean || an object with details about the failures
+   
     return failures;
 }
 
